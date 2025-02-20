175859acd3b7ef2e9cc00e639297fc69dfb0b8e5
­Jason Prime <600613.m6@gmail.com>
2025-02-20 22:12:40 +0100

    Update prevent_sleep.sh
    Comments and Descriptions added

--- prevent_sleep.sh
+++ prevent_sleep.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# Jan Gebser - Brainhub24.com
+# Email: github@brainhub24.com
+# About this Tool:
+# - It is a versaitel utility tool to improve the proxmox configurations hosted on a regular laptop device.
+# Improvements:
+# - Preventing the laptop to fall asleep
+# - Added Comments  
 
-# Path to the logind.conf file
+# Path to the  logind.conf file
 LOGIND_CONF="/etc/systemd/logind.conf"
 
 # Backup the current logind.conf file
-cp "$LOGIND_CONF" "$LOGIND_CONF.bak"
+cp "$LOGIND_CONF" "LOGIND_CONF.bak"
 
 # Add the required lines to logind.conf
 echo "HandleLidSwitch=ignore" >> "$LOGIND_CONF"
